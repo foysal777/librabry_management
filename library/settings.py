639a2d463b0440878753aaaae1e92aafec8ee620
foysal777 <foysal.cse11@gmail.com>
2024-06-24 21:25:35 +0600

    csrf token

--- library/settings.py
+++ library/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-(=us(pg0+gec9@=a^$z63zdsw2pq0_t5ohz=v6-u(d(rd_q%w4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
